59af079299c9bf457a3a382385b10e183e2fdf8d
Taner Çam <tanercamm@outlook.com>
2024-08-18 15:40:57 +0300

    Add Entities

---
+++ NTierManagement.Entity/Models/Persons/Leader.cs
@@ -0,0 +1,16 @@
+﻿using NTierManagement.Entity.Models.Workplace;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NTierManagement.Entity.Models.Person
+{
+    public class Leader : Person
+    {
+        [Required]
+        public int DepartmentID { get; set; }
+
+        [ForeignKey("DepartmentID")]
+        public Department Department { get; set; }
+
+    }
+}
---
+++ NTierManagement.Entity/Models/Persons/Person.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace NTierManagement.Entity.Models.Person
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+
+        [StringLength(30)]
+        public string FirstName { get; set; }
+
+        [StringLength(15)]
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
